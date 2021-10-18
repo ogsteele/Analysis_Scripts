9a7422f704767a15e252dae03ef3beeb85efe575
Oliver Steele <69857520+ogsteele@users.noreply.github.com>
2021-10-18 16:06:13 +0100

    Update TP_null_MedianF.m

--- mEPSC/TP_null_MedianF.m
+++ mEPSC/TP_null_MedianF.m
@@ -101,23 +101,38 @@ clear('title_str')
 close
 
 %% exlcudedata, filter and restitch
-time = 
-for i = 1:size(splits,2)
+nf_splits = splits;
 
-    
-% select the range to exclude
-% tf = excludedata(ydata,xdata,'range',[100, 150]);
-tf = excludedata(array,time,'range',[x(1), 1]); % not excl.
-tx = ~excludedata(array,time,'range',[0.94, 1]); % excl.
+for i = size(splits,2)
+    array = splits(:,i);
+    time = 0:5.0000e-05:Param.split_length;
+    time = time(1:Param.sample_rate*Param.split_length)';
+
+    % select the range to exclude
+    % tf = excludedata(ydata,xdata,'range',[100, 150]);
+    range_dp = [x(1), x(1)+(0.035*Param.sample_rate)];
+    range_s = range_dp/Param.sample_rate;
+    tf = excludedata(array,time,'range',range_s); % not excl.
+    tx = ~excludedata(array,time,'range',range_s); % excl.
+
+    % filter the region, bar exclusions
+    yf = medianf(array(tf), time(tf) ,11); % filtered array
+
+    % now add back in the bit you didn't want to be filtered
+    full = vertcat(yf(1:x(1)),array(tx),yf((x(1)):end));
+
+    splits(:,i) = full;
+end
+
 % plot region with exclusion zone
 plot(time(tf),array(tf))
 % plot the excluded region over the top (another color by default
 hold on; plot(time(tx),array(tx))
 title('Region excluded from median filtering')
-
-% filter the region, bar exclusions
-yf = medianf(array(tf), time(tf) ,11); % filtered array
-
-% now add back in the bit you didn't want to be filtered
-full = vertcat(yf(1:0.94*S_Hz),array(tx),yf((0.94*S_Hz)+1:end));
 figure; plot(full)
+
+figure; plot(tf); hold on; plot(tx); ylim([-0.5 1.5]); hold on
+legend('tf','tx')
+xline(range_dp(1)); hold on; xline(range_dp(2)); hold off
+legend('tf','tx')
+title('tf / tx')
