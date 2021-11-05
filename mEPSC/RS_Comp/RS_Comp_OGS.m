3cea05ad35931f4ac156132ad61ac42c71f22c25
Oliver Steele <69857520+ogsteele@users.noreply.github.com>
2021-11-05 08:13:25 +0000

    Rs_Comp_OGS work

--- mEPSC/RS_Comp/RS_Comp_OGS.m
+++ mEPSC/RS_Comp/RS_Comp_OGS.m
@@ -567,3 +567,55 @@ x2 = linspace(0,(size(tprm_splits_conc,1)/20000),size(median_YF,1));
 tprm = YF;
 med = median_YF;
 end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function [splits, nf_splits] = TP_null_MedianF_function(Param,splits,x,poles)
+
+% aim is to median filter a recording without the selected region being
+% median filtered
+
+% exlcudedata, filter and restitch
+% % warning off
+warning('off','MATLAB:colon:nonIntegerIndex')
+nf_splits = splits;
+
+for i = 1:size(splits,2)
+    array = splits(:,i);
+    time = 0:5.0000e-05:Param.split_length;
+    time = time(1:Param.sample_rate*Param.split_length)';
+
+    % select the range to exclude
+    % tf = excludedata(ydata,xdata,'range',[100, 150]);
+    range_dp = [x(1), x(1)+(0.035*Param.sample_rate)];
+    range_s = range_dp/Param.sample_rate;
+    
+    tf = excludedata(array,time,'range',range_s); % not excl.
+    tx = ~excludedata(array,time,'range',range_s); % excl.
+    
+    % filter the region, bar exclusions
+    yf = medianf(array(tf), time(tf) ,poles); % filtered array
+    
+    % now add back in the bit you didn't want to be filtered
+    full = vertcat(yf(1:x(1)),array(tx),yf((x(1)):end));
+    splits(:,i) = full;
+end
+
+% bug testing plots
+% plot region with exclusion zone
+% % plot(time(tf),array(tf))
+% % plot the excluded region over the top (another color by default
+% % hold on; plot(time(tx),array(tx))
+% % title('Region excluded from median filtering')
+% % figure; plot(full)
+% % 
+% % figure; plot(tf); hold on; plot(tx); ylim([-0.5 1.5]); hold on
+% % legend('tf','tx')
+% % xline(range_dp(1)); hold on; xline(range_dp(2)); hold off
+% % legend('tf','tx')
+% % title('tf / tx')
+end
