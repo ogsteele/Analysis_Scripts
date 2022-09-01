cbe79dd61965a7a8e1c1e75e3ad00d779572d969
Oliver Steele <69857520+ogsteele@users.noreply.github.com>
2022-09-01 08:21:23 +0100

    Update VStep_compiler.m

--- Basic Properties Toolbox/Sussex/VStep_compiler.m
+++ Basic Properties Toolbox/Sussex/VStep_compiler.m
@@ -36,10 +36,10 @@ E3_control = compiled(strcmp({compiled.Genotype}, 'APOE3') ...
     & strcmp({compiled.Ketamine}, 'No') & strcmp({compiled.Memantine}, 'No'));
 E3_ketamine = compiled(strcmp({compiled.Genotype}, 'APOE3') ...
     & strcmp({compiled.Ketamine}, 'Yes') & strcmp({compiled.Memantine}, 'No'));
-%E3_memantine = compiled(strcmp({compiled.genotype}, 'APOE3') ...
-%    & strcmp({compiled.ketamine}, 'No') & strcmp({compiled.memantine}, 'Yes'));
-%E4_memantine = compiled(strcmp({compiled.genotype}, 'APOE4') ...
-%    & strcmp({compiled.ketamine}, 'No') & strcmp({compiled.memantine}, 'Yes'));
+E3_memantine = compiled(strcmp({compiled.genotype}, 'APOE3') ...
+    & strcmp({compiled.ketamine}, 'No') & strcmp({compiled.memantine}, 'Yes'));
+E4_memantine = compiled(strcmp({compiled.genotype}, 'APOE4') ...
+    & strcmp({compiled.ketamine}, 'No') & strcmp({compiled.memantine}, 'Yes'));
 
 %% Calculate Maximum Current Densities
 
@@ -60,6 +60,13 @@ E3K_KD(i) = (max(E3_ketamine(i).KMean)*1e12)/E3_ketamine(i).Cap;
 E3K_NaID(i) = (min(E3_ketamine(i).NaAPeak)*1e12)/E3_ketamine(i).Cap;
 end
 
+% APOE3 Memantine
+for i = 1:size(E3_memantine,2)
+E3M_NaAD(i) = (min(E3_memantine(i).NaAPeak)*1e12)/E3_memantine(i).Cap;
+E3M_KD(i) = (max(E3_memantine(i).KMean)*1e12)/E3_memantine(i).Cap;
+E3M_NaID(i) = (min(E3_memantine(i).NaAPeak)*1e12)/E3_memantine(i).Cap;
+end
+
 ind = [];
 % APOE4 Control
 for i = 1:size(E4_control,2)
@@ -70,14 +77,21 @@ E4C_NaID(i) = (min(E4_control(i).NaAPeak)*1e12)/E4_control(i).Cap;
 E4C_peak_array(:,i) = E4_control(i).Waves(:,ind(i))/E4_control(i).Cap;
 end
 
-% APOE4 Ketamine
+% APOE4 Memantine
 for i = 1:size(E4_ketamine,2)
 E4K_NaAD(i) = (min(E4_ketamine(i).NaAPeak)*1e12)/E4_ketamine(i).Cap;
 E4K_KD(i) = (max(E4_ketamine(i).KMean)*1e12)/E4_ketamine(i).Cap;
 E4K_NaID(i) = (min(E4_ketamine(i).NaAPeak)*1e12)/E4_ketamine(i).Cap;
 end
 
-%% plot maximum sodium activation current densities
+% APOE4 Ketamine
+for i = 1:size(E4_memantine,2)
+E4M_NaAD(i) = (min(E4_memantine(i).NaAPeak)*1e12)/E4_memantine(i).Cap;
+E4M_KD(i) = (max(E4_memantine(i).KMean)*1e12)/E4_memantine(i).Cap;
+E4M_NaID(i) = (min(E4_memantine(i).NaAPeak)*1e12)/E4_memantine(i).Cap;
+end
+
+%% plot maximum sodium activation current densities +/- ketamine
 colorscheme = ...
     [0 0 0; ...
     0.6350 0.0780 0.1840];
@@ -150,7 +164,80 @@ sgtitle('Maximal NaA Current Density:')
     title('APOE3 vs APOE4 with ketamine')
     ylim([-400 0])
 
-%% plot maximum sodium inactivation current densities
+%% plot maximum sodium activation current densities +/- ketamine
+colorscheme = ...
+    [0 0 0; ...
+    0.6350 0.0780 0.1840];
+NaA_mem_fig = figure;
+sgtitle('Maximal NaA Current Density:')
+
+    % APOE3 vs APOE4
+    labels = {'E3 Control', 'E4 Control'};
+    subplot(1,4,1)
+    Y = padcat(E3C_NaAD', E4C_NaAD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylim([-400 0])
+
+    % APOE3 with and without memantine
+    labels = {'E3 Control', 'E3 Memantine'};
+    subplot(1,4,2)
+    Y = padcat(E3C_NaAD', E3M_NaAD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)')
+    title('APOE3 with and without memantine')
+    ylim([-400 0])
+
+    % APOE4 with and without Memantine
+    labels = {'E4 Control', 'E4 Memantine'};
+    subplot(1,4,3)
+    Y = padcat(E4C_NaAD', E4M_NaAD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)')
+    title('APOE4 with and without memantine')
+    ylim([-400 0])
+
+    % APOE3/4 with memantine
+    labels = {'E3 Controol', 'E4 Ketamine'};
+    subplot(1,4,4)
+    Y = padcat(E3C_NaAD', E4M_NaAD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)');
+    title('APOE3 vs APOE4 with memantine')
+    ylim([-400 0])
+
+%% plot maximum sodium inactivation current densities +/- ketamine
 NaI_ket_fig = figure;
 sgtitle('Maximal NaI Current Density:')
 
@@ -222,7 +309,79 @@ sgtitle('Maximal NaI Current Density:')
     title('APOE3 vs APOE4 with ketamine')
     ylim([-400 0])
 
-%% plot maximum potassium current densities
+%% plot maximum sodium inactivation current densities +/- memantine
+NaI_mem_fig = figure;
+sgtitle('Maximal NaI Current Density:')
+
+    % APOE3 vs APOE4
+    labels = {'E3 Control', 'E4 Control'};
+    subplot(1,4,1)
+    Y = padcat(E3C_NaID', E4C_NaID');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)');
+    title('APOE3 vs APOE4')
+    ylim([-400 0])
+
+    % APOE3 with and without memantine
+    labels = {'E3 Control', 'E3 Memantine'};
+    subplot(1,4,2)
+    Y = padcat(E3C_NaID', E3M_NaID');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)')
+    title('APOE3 with and without Memantine')
+    ylim([-400 0])
+
+    % APOE4 with and without Memantine
+    labels = {'E4 Control', 'E4 Memantine'};
+    subplot(1,4,3)
+    Y = padcat(E4C_NaID', E4M_NaID');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)')
+    title('APOE4 with and without Memantine')
+    ylim([-400 0])
+
+    % APOE3/4 with Memantine
+    labels = {'E3 Control', 'E4 Memantine'};
+    subplot(1,4,4)
+    Y = padcat(E3C_NaID', E4M_NaID');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)');
+    title('APOE3 vs APOE4 with Memantine')
+    ylim([-400 0])
+
+%% plot maximum potassium current densities +/- ketamine
 K_ket_fig = figure;
 sgtitle('Maximal K Current Density:')
 
@@ -294,8 +453,80 @@ sgtitle('Maximal K Current Density:')
     title('APOE3 vs APOE4 with ketamine')
     ylim([0 200])
 
-%% Current voltage relationships
+%% plot maximum potassium current densities +/- Memantine
+K_ket_fig = figure;
+sgtitle('Maximal K Current Density:')
 
+    % APOE3 vs APOE4
+    labels = {'E3 Control', 'E4 Control'};
+    subplot(1,4,1)
+    Y = padcat(E3C_KD', E4C_KD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)');
+    title('APOE3 vs APOE4')
+    ylim([0 200])
+
+    % APOE3 with and without Memantine
+    labels = {'E3 Control', 'E3 Memantine'};
+    subplot(1,4,2)
+    Y = padcat(E3C_KD', E3M_KD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)')
+    title('APOE3 with and without Memantine')
+    ylim([0 200])
+
+    % APOE4 with and without Memantine
+    labels = {'E4 Control', 'E4 Memantine'};
+    subplot(1,4,3)
+    Y = padcat(E4C_KD', E4M_KD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)')
+    title('APOE4 with and without Memantine')
+    ylim([0 200])
+
+    % APOE3/4 with ketamine
+    labels = {'E3 Control', 'E4 Memantine'};
+    subplot(1,4,4)
+    Y = padcat(E3C_KD', E4M_KD');
+    b = boxplot(Y, 'labels', labels);
+    box off; set(gca,'linewidth',2); set(gcf,'color','white');
+    set(b(7,:),'Visible','off') % make the outlier points invisible
+    x=repmat(1:4,length(Y),1);
+    hold on
+    % plot individual values
+    for i = 1:size(Y,2)
+        scatter(x(:,i),Y(:,i),'filled','MarkerFaceAlpha',0.6','jitter','on','jitterAmount',0.15,'MarkerFacecolor',colorscheme(i,:));
+    end
+    ylabel('Current Density (pA/pF)');
+    title('APOE3 vs APOE4 with Memantine')
+    ylim([0 200])
+
+%% Current voltage relationships
+% note, not sure what these are truly supposed to be
 
 %% save figures and output
 
@@ -305,10 +536,23 @@ cd('VStep Analysis')
 % structure
 vals.E3C_NaAD = E3C_NaAD;
 vals.E4C_NaAD = E4C_NaAD;
+vals.E3K_NaAD = E3K_NaAD;
+vals.E4K_NaAD = E4K_NaAD;
+vals.E3M_NaAD = E3M_NaAD;
+vals.E4M_NaAD = E4M_NaAD;
 vals.E3C_KD = E3C_KD;
 vals.E4C_KD = E4C_KD;
+vals.E3K_KD = E3K_KD;
+vals.E4K_KD = E4K_KD;
+vals.E3M_KD = E3M_KD;
+vals.E4M_KD = E4M_KD;
 vals.E3C_peak_array = E3C_peak_array;
 vals.E4C_peak_array = E4C_peak_array;
+vals.E3K_peak_array = E3K_peak_array;
+vals.E4K_peak_array = E4K_peak_array;
+vals.E3M_peak_array = E3M_peak_array;
+vals.E4M_peak_array = E4M_peak_array;
+
 
 if save_struct == true
 save('compiled.mat', 'compiled', '-v7.3')
